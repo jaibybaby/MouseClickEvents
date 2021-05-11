1566b31e97fbe96be000db25525183d764579f35
jaibymol4@gmail.com <jaibymol4@gmail.com>
2021-05-11 19:54:26 +0530

    Add project files.

---
+++ MouseClickEvents/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace MouseClickEvents
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtHeading = new System.Windows.Forms.TextBox();
+            this.picBall = new System.Windows.Forms.PictureBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.panel1.Controls.Add(this.picBall);
+            this.panel1.Location = new System.Drawing.Point(12, 103);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 417);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtHeading
+            // 
+            this.txtHeading.BackColor = System.Drawing.Color.Yellow;
+            this.txtHeading.Location = new System.Drawing.Point(12, 73);
+            this.txtHeading.Multiline = true;
+            this.txtHeading.Name = "txtHeading";
+            this.txtHeading.Size = new System.Drawing.Size(743, 33);
+            this.txtHeading.TabIndex = 0;
+            this.txtHeading.Text = "Running Ball";
+            this.txtHeading.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // picBall
+            // 
+            this.picBall.Image = global::MouseClickEvents.Properties.Resources.ball2;
+            this.picBall.Location = new System.Drawing.Point(347, 154);
+            this.picBall.Name = "picBall";
+            this.picBall.Size = new System.Drawing.Size(120, 104);
+            this.picBall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBall.TabIndex = 2;
+            this.picBall.TabStop = false;
+            this.picBall.MouseClick += new System.Windows.Forms.MouseEventHandler(this.picBall_MouseClick);
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Yellow;
+            this.btnClose.Location = new System.Drawing.Point(751, 71);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(37, 35);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 555);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtHeading);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Running Ball";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtHeading;
+        private System.Windows.Forms.PictureBox picBall;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ MouseClickEvents/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseClickEvents
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void picBall_MouseClick(object sender, MouseEventArgs e)
+        {
+            int x = picBall.Location.X;
+            int y = picBall.Location.Y;
+            if (e.Button == MouseButtons.Left)
+            {
+                picBall.Location = new Point(picBall.Location.X-20,picBall.Location.Y);
+                // this.Clicks = picBall.Left;
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                picBall.Location = new Point(picBall.Location.X + 20, picBall.Location.Y);
+            }
+           
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
